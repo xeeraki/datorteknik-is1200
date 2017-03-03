37a419b34efafd80f312c5e8b3793deafc3a5e94
xeeraki <shafai86@gmail.com>
2017-03-03 10:29:43 +0100

    Completed

--- timetemplate.asm
+++ timetemplate.asm
@@ -24,7 +24,7 @@ main:
 	syscall
 	nop
 	# wait a little
-	li	$a0,30000
+	li	$a0,300000		#argument to delay (initialy was set to 2)
 	jal	delay
 	nop
 	# call tick
@@ -73,19 +73,18 @@ tiend:	sw	$t0,0($a0)	# save updated result
 	nop
 
   # you can write your code for subroutine "hexasc" and  below this line
-  #
 hexasc:
    	andi	$a0,$a0,0xf		#only 4 least significant bits ignore(clear) other bits	
-   	addi	$v0,$zero,0x30	        #$v0 = 0x30 ('0' ASCII character)
-   	addi	$t0,$zero,0x9	        #t0 = 0x9
+   	addi	$v0,$zero,0x30	#$v0 = 0x30 ('0' ASCII character)
+   	addi	$t0,$zero,0x9	#t0 = 0x9
    	
    	ble	$a0,$t0,L1		#branch to L1 if a0 <= t0
    	nop
    	addi	$v0,$v0,0x7		#v0 = v0 +0x7 (thier is 7 other characters from 0x39 to 0x41) thats why 
-					# 0x7 is added to the v0 to ignore the gap and print firectly A
+					#0x7 is added to the v0 to ignore the gap and jump directly to 'A' in ASCII table
    	
    L1:
-   	add	$v0,$a0,$v0 	#v0 = V0 +a0 (if 
+   	add	$v0,$a0,$v0 	#v0 = V0 +a0 
    	jr	$ra
    	nop
 
@@ -93,17 +92,19 @@ hexasc:
 delay:
 	PUSH	($s0)
 	PUSH	($ra)
-	addi	$s0, $0,0		#i = 0
-	addi	$t1,$0,4711
+	addi	$s0, $0,1000	#i = 0
+	addi	$t1,$0,30000   	#(intitaly was t1=4711)
 	move	$t0,$a0		#move the argument to t0
 while:
-	bgt	$t0,$s0,done	#branch to done if ms > 0
+	ble	$t0,$s0,done	#branch to done if ms > 0
+	nop
 	addi	$t0,$t0,-1		#decrement ms by 1	
 for:
-	beq	$s0,$t1,done
+	beq	$s0,$t1,done2
+	nop
 	addi	$s0,$s0,1
 	j	for
-	
+done2:	
 	
 	j	while
 done:	
@@ -119,22 +120,22 @@ done:
  	PUSH	($s1)
  	PUSH	($ra)			#nested subroutine must store $ra too
  	
- 	add	$s0,$0,$a0		#contaisn the adress of string (timstr)
+ 	add	$s0,$0,$a0		#contains the adress of string (timstr)
  	add	$s1,$0,$a1		#contains the time-info(0x5957)
  	
- 	andi	$t0,$s1,0xf000  	#check the 4 most signifaicant bits ignore other bits
+ 	andi	$t0,$s1,0xf000  	#check the 4 most signifaicant bits ignore(clear) other bits
  	srl	$a0,$t0,12		#shift the MSB to LSB position (hexasc take only 4 bits in the LSB position)
  	jal	hexasc		# call hexasc
  	nop		
  	sb	$v0, 0($s0)		#stor that 4 bits in that location that a0 points to
  	
- 	andi	$t1,$s1,0x0f00	#mask to get those 4 bits you and ignore other bits
+ 	andi	$t1,$s1,0x0f00	#mask to get those 4 bits you need and ignore other bits
  	srl	$a0,$t1,8		#shift those bits to the LSB position(0x000f)
  	jal	hexasc		#this is becouse hexasc only take argument on the LSB postion and clear other bits.
  	nop
- 	sb	$v0,1($s0)		
+ 	sb	$v0,1($s0)		#store this bits in that address pointed by a0 
  	
- 	li	$t3,0x3A
+ 	li	$t3,0x3A		#t3 = a colon in ASCII table 
  	sb	$t3,2($s0)
  	
  	
@@ -151,7 +152,7 @@ done:
  	sb	$v0, 4($s0)
  	
  	
- 	li	$t4,0x00
+ 	li	$t4,0x00		# a null \0 in ASCII table
  	sb	$t4,5($s0)
  	
  
