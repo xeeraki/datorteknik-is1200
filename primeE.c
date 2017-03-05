09fcb3da8d1d0e66c5cbcb3b17510a97b9da82f4
xeeraki <shafai86@gmail.com>
2017-03-05 09:34:14 +0100

    new

---
+++ primeE.c
@@ -0,0 +1,29 @@
+
+/*
+ prime.c
+ By David Broman.
+ Last modified: 2015-09-15
+ This file is in the public domain.
+*/
+
+
+#include <stdio.h>
+
+int is_prime(int n){
+	//my code begins here
+int p = 2;	
+	 for(int i = 2; i <n; i++)
+ 	{
+ 	
+ 	if()
+ 		p = n;
+ 	}
+  return 1;
+  //my code finish here
+}
+
+int main(void){
+  printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
+  printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
+  printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
+}
