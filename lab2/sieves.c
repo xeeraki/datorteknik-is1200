c49c759460a77eab1df7c76bff4b7b61e45edc2f
xeeraki <shafai86@gmail.com>
2017-03-15 18:42:35 +0100

    update

--- lab2/sieves.c
+++ lab2/sieves.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define COLUMNS 6
-#define MAX_PRIMES 105
+#define MAX_PRIMES 500
 
-void print_sieves(int max[]){
-	int max = [MAX_PRIMES];
+void print_sieves(int n){
+	int i, j, max [MAX_PRIMES];
 	//samllest_prime is p = 2
 	int p = 2;
-	for(int i = 2; i < MAX_PRIMES;i++){
+	for(i = 2; i < n;i++){
 		//if p is multiple of 2p, 3p ... mark them out but not p it self
-		if((max[i]*p)%2 == 0){
-			max[i]= 2;
-			// look for the next i value which is not multiple of p 
-		else if((max[i] * p)%2 == 1){
-			p++;
-			print_number(p);
+		if((max[i] = max[i]*p)){
+			max[i] = 0;
 			}
-		} 
-	}
-}
+		}
+		// look for the next i value which is not multiple of p 
+		for(j = i*i; j <n; j+=i){
+			max[j]=1;
+			print_number(max[j]);
+			printf("\n");
+		}
+	} 		
+	
+	
+	
 void print_number(int n){
 	int i;
 		printf("%10d\t",n);
@@ -29,10 +33,9 @@ void print_number(int n){
 }
 int main(int argc, char *argv[]){
   if(argc == 2)
-    print_primes(atoi(argv[1]));
+    print_sieves(atoi(argv[1]));
   else
     printf("Please state an interger number.\n");
-    //print_primes(105);
   return 0;
   
 }
