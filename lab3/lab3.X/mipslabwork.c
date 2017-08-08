3ea251c1af6b291f40602e38361bbcc45e413605
xeeraki <shafai86@gmail.com>
2017-08-08 14:47:03 +0200

    lab3

---
+++ lab3/lab3.X/mipslabwork.c
@@ -0,0 +1,52 @@
+/* 
+ * File:   mipslabwork.c
+ * Author: shafai
+ *
+ * Created on August 5, 2017, 7:15 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* mipslabwork.c
+
+   This file written 2015 by F Lundevall
+
+   This file should be changed by YOU! So add something here:
+
+   This file modified 2015-12-24 by Ture Teknolog 
+
+   Latest update 2015-08-28 by F Lundevall
+
+   For copyright and licensing, see file COPYING */
+
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <p32xxxx.h>  /* Declarations of system-specific addresses etc */
+#include "mipslab.h"  /* Declatations for these labs */
+
+int mytime = 0x5957;
+
+char textstring[] = "text, more text, and even more text!";
+
+/* Interrupt Service Routine */
+void user_isr( void )
+{
+  return;
+}
+
+/* Lab-specific initialization goes here */
+void labinit( void )
+{
+  return;
+}
+
+/* This function is called repetitively from the main program */
+void labwork( void )
+{
+  delay( 1000 );
+  time2string( textstring, mytime );
+  display_string( 3, textstring );
+  display_update();
+  tick( &mytime );
+  display_image(96, icon);
+}
